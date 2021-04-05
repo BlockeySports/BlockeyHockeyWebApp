f4a311bc74fb9185290dfa94d488698199dec6b6
harvanchik <66141166+harvanchik@users.noreply.github.com>
2021-04-04 21:55:49 -0700

    logo and title link to home page

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,14 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
-  selector: 'app-header',
-  templateUrl: './header.component.html'
+    selector: 'app-header',
+    templateUrl: './header.component.html'
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+    constructor(
+        private router: Router
+    ) { }
 
-  ngOnInit(): void {
-  }
+    ngOnInit(): void {
+    }
+
+    public navigateHome(): void {
+        this.router.navigate(['/']);
+    }
 
 }
