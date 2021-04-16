afd2f7f4f07e9bdd43aab407bec1481b5d6988f1
harvanchik <66141166+harvanchik@users.noreply.github.com>
2021-04-15 18:02:42 -0700

    switch stats and member into profile object

---
+++ src/app/models/HockeyStats.ts
@@ -0,0 +1,15 @@
+export class HockeyStats {
+    global: {
+        gamesPlayed?: number,
+        wins?: number,
+        losses?: number,
+        overtimeWins?: number,
+        overtimeLosses?: number,
+        forfeits?: number,
+        goals?: number,
+        primaryAssists?: number,
+        secondaryAssists?: number,
+        shots?: number,
+        timeOnIce?: number,
+    };
+}
---
+++ src/app/models/Profile.ts
@@ -0,0 +1,8 @@
+
+import { HockeyStats } from './HockeyStats';
+import { Member } from './Member';
+
+export class Profile {
+    member?: Member;
+    stats?: HockeyStats;
+}
