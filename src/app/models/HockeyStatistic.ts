dded3ad0483313e8fdc1316b064aae9181e349e0
harvanchik <66141166+harvanchik@users.noreply.github.com>
2021-04-20 18:00:40 -0700

    refactored hockeystats to hockeystatistic

---
+++ src/app/models/HockeyStatistic.ts
@@ -0,0 +1,18 @@
+export class HockeyStatistic {
+    description?: string;
+    order?: number;
+
+    gamesPlayed?: number;
+    wins?: number;
+    losses?: number;
+    overtimeWins?: number;
+    overtimeLosses?: number;
+    forfeitWins?: number;
+    forfeitLosses?: number;
+    goals?: number;
+    goalsAgainst?: number;
+    primaryAssists?: number;
+    secondaryAssists?: number;
+    shots?: number;
+    timeOnIce?: number;
+}
