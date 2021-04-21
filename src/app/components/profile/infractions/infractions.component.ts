a688accafb8658589ab461d9b02933cacdc5cb9b
harvanchik <66141166+harvanchik@users.noreply.github.com>
2021-04-20 22:59:41 -0700

    temporary method to get username of punisher from uuid

--- src/app/components/profile/infractions/infractions.component.ts
+++ src/app/components/profile/infractions/infractions.component.ts
@@ -1,17 +1,53 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { AfterViewChecked, AfterViewInit, Component, Input, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { take } from 'rxjs/operators';
 import { Punishment } from 'src/app/models/Punishment';
+import { PlayerService } from 'src/app/services/player.service';
 
 @Component({
-  selector: 'app-infractions',
-  templateUrl: './infractions.component.html'
+    selector: 'app-infractions',
+    templateUrl: './infractions.component.html'
 })
-export class InfractionsComponent implements OnInit {
+export class InfractionsComponent implements OnInit, AfterViewInit, OnDestroy {
 
-  @Input() punishments: Punishment[];
+    @Input() punishments: Punishment[];
 
-  constructor() { }
+    // Subs
+    private playerSub: Subscription;
 
-  ngOnInit(): void {
-  }
+    constructor(
+        private playerService: PlayerService
+    ) { }
+
+    ngOnInit(): void {
+    }
+
+    ngAfterViewInit(): void {
+        setTimeout(
+            () => {
+                for (const punishment of this.punishments) {
+                    this.getUsernameFromUuid(punishment);
+                }
+            }, 500
+        );
+
+    }
+
+    // TODO: make punisher and punished Member objects instead of just uuids
+    public getUsernameFromUuid(punishment: Punishment): void {
+        this.playerSub = this.playerService.getPlayerInfo(punishment.punisher).subscribe(
+            (data) => {
+                punishment.punisher = data.username;
+            },
+            (error) => {
+                console.log(error);
+                punishment.punisher = 'Error';
+            }
+        );
+    }
+
+    ngOnDestroy(): void {
+        if (this.playerSub) { this.playerSub.unsubscribe(); }
+    }
 
 }
