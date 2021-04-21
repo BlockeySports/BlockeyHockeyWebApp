06b259724817ad0aa594123c89b30f68c590740c
harvanchik <66141166+harvanchik@users.noreply.github.com>
2021-04-20 23:02:32 -0700

    moved tippy logic for date joined to stats component

--- src/app/components/profile/stats/stats.component.ts
+++ src/app/components/profile/stats/stats.component.ts
@@ -1,5 +1,5 @@
 import { formatDate } from '@angular/common';
-import { Component, Input, OnInit } from '@angular/core';
+import { AfterViewInit, Component, Input, OnInit } from '@angular/core';
 import { NgxTippyService } from 'ngx-tippy-wrapper';
 import { Member } from 'src/app/models/Member';
 
@@ -7,13 +7,25 @@ import { Member } from 'src/app/models/Member';
     selector: 'app-stats',
     templateUrl: './stats.component.html'
 })
-export class StatsComponent implements OnInit {
+export class StatsComponent implements OnInit, AfterViewInit {
 
     @Input() member: Member;
 
     constructor(
+        private tippyService: NgxTippyService
     ) { }
 
     ngOnInit(): void {
+    }
+
+    ngAfterViewInit(): void {
+        setTimeout(
+            () => {
+                const content = this.member.dateJoined
+                    ? 'First seen on ' + formatDate(this.member.dateJoined, 'MMM d, y, h:mm a', 'en-US')
+                    : 'N/A';
+                this.tippyService.setContent('first-seen', content);
+            }, 500
+        );
     }
 }
