feca0890239ef8797b0499234323467c708c0ef5
harvanchik <66141166+harvanchik@users.noreply.github.com>
2021-05-27 20:34:10 -0700

    added boxscore component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
+import { BoxScoreComponent } from './components/boxscore/boxscore.component';
 import { HomeComponent } from './components/home/home.component';
 import { ProfileComponent } from './components/profile/profile.component';
 
 const routes: Routes = [
   { path: 'u/:username', component: ProfileComponent },
+  { path: 'boxscore', component: BoxScoreComponent },
   { path: '', component: HomeComponent }
 ];
 
---
+++ src/app/components/boxscore/boxscore.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-boxscore',
+  templateUrl: './boxscore.component.html'
+})
+export class BoxScoreComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
