b5790d0b351479e8480d21f760d4eddb56e649a7
harvanchik <66141166+harvanchik@users.noreply.github.com>
2021-05-27 23:05:11 -0700

    fixed tailwind config missing custom values

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,36 @@
 module.exports = {
     mode: 'jit',
     prefix: '',
-    purge: {
-        content: [
-            './src/**/*.{html,ts}',
-        ]
-    },
+    purge: ["./src/**/*.{html,ts}"],
     darkMode: 'class', // or 'media' or 'class'
     theme: {
-        extend: {},
+        extend: {
+            fontFamily: {
+                'sans': ["'Noto Sans'", 'sans-serif']
+            },
+            colors: {
+                'black': {
+                    DEFAULT: '#000000',
+                    '50': '#3C3C3C',
+                    '100': '#383838',
+                    '200': '#303030',
+                    '300': '#292929',
+                    '400': '#212121',
+                    '500': '#1A1A1A',
+                    '600': '#161616',
+                    '700': '#121212',
+                    '800': '#0A0A0A',
+                    '900': '#030303'
+                },
+            },
+        },
     },
     variants: {
         extend: {},
     },
-    plugins: [require('@tailwindcss/aspect-ratio'), require('@tailwindcss/forms'), require('@tailwindcss/line-clamp')],
+    plugins: [
+        require('@tailwindcss/aspect-ratio'),
+        require('@tailwindcss/forms'),
+        require('@tailwindcss/line-clamp'),
+        require('tailwind-scrollbar')],
 };
