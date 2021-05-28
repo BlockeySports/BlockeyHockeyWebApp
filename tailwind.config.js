50ea1a477001f22916503d0ab986a953faf2f68f
harvanchik <66141166+harvanchik@users.noreply.github.com>
2021-05-27 21:31:04 -0700

    updated tailwind and using JIT engine

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+    mode: 'jit',
+    prefix: '',
+    purge: {
+        content: [
+            './src/**/*.{html,ts}',
+        ]
+    },
+    darkMode: 'class', // or 'media' or 'class'
+    theme: {
+        extend: {},
+    },
+    variants: {
+        extend: {},
+    },
+    plugins: [require('@tailwindcss/aspect-ratio'), require('@tailwindcss/forms'), require('@tailwindcss/line-clamp')],
+};
