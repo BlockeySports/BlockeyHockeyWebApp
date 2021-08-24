450f3e0843e2db76fc85f42fb5059ebb963483c8
harvanchik <66141166+harvanchik@users.noreply.github.com>
2021-08-24 14:55:58 -0700

    fixed infractions length duration incorrect

--- src/app/pipes/length.pipe.ts
+++ src/app/pipes/length.pipe.ts
@@ -1,19 +1,25 @@
 import { Pipe, PipeTransform } from '@angular/core';
-import * as timeago from 'timeago.js';
+import { Dayjs } from 'dayjs';
+import dayjs from 'dayjs';
+import duration from 'dayjs/plugin/duration';
+import relativeTime from 'dayjs/plugin/relativeTime';
 
 @Pipe({
   name: 'length'
 })
 export class LengthPipe implements PipeTransform {
 
-  transform(length: number, date: Date): string {
+  transform(length: number): string {
     if (length < 0) {
       return 'Forever';
     }
-    if (length <= 10) {
+    if (length === 0) {
       return 'None';
     }
-    const duration = timeago.format(new Date(new Date(date).getTime() + length));
-    return duration.replace('in ', '');
+    // extend the plugins for this to work
+    dayjs.extend(duration);
+    dayjs.extend(relativeTime);
+    // make the duration display in a human readable way
+    return dayjs.duration(length).humanize();
   }
 }
