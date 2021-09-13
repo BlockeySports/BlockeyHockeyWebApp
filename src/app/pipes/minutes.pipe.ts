0c943b32a39b861d17cee80edd06ebaa2b93c7fc
harvanchik <66141166+harvanchik@users.noreply.github.com>
2021-09-12 22:42:16 -0700

    if null input, return empty string

--- src/app/pipes/minutes.pipe.ts
+++ src/app/pipes/minutes.pipe.ts
@@ -9,7 +9,7 @@ export class MinutesPipe implements PipeTransform {
    * Convert milliseconds to minutes.
    */
   transform(milliseconds: number): string {
-    if (!milliseconds) { milliseconds = 0; }
+    if (!milliseconds) { return ''; }
     const minutes = Math.floor(milliseconds / 60000);
     // return minutes with correct pluralization
     return minutes === 1 ? '1 minute' : `${minutes} minutes`;
