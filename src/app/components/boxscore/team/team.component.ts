e7337ec4c1e954a4d6e2d9cc6faaa10fb10cf94f
harvanchik <66141166+harvanchik@users.noreply.github.com>
2021-09-12 22:42:28 -0700

    use single quotes instead

--- src/app/components/boxscore/team/team.component.ts
+++ src/app/components/boxscore/team/team.component.ts
@@ -6,13 +6,13 @@ import { HockeyTeam } from 'src/app/models/HockeyTeam';
     selector: 'app-team',
     templateUrl: './team.component.html',
     animations: [
-        trigger("toggleAnimation", [
-            transition(":enter", [
-                style({ opacity: 0, transform: "scale(0.95)" }),
-                animate("100ms ease-out", style({ opacity: 1, transform: "scale(1)" }))
+        trigger('toggleAnimation', [
+            transition(':enter', [
+                style({ opacity: 0, transform: 'scale(0.95)' }),
+                animate('100ms ease-out', style({ opacity: 1, transform: 'scale(1)' }))
             ]),
-            transition(":leave", [
-                animate("75ms", style({ opacity: 0, transform: "scale(0.95)" }))
+            transition(':leave', [
+                animate('75ms', style({ opacity: 0, transform: 'scale(0.95)' }))
             ])
         ])
     ]
