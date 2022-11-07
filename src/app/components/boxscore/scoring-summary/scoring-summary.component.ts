44395668a2dae9f71328e86afad62eea833cf699
harvanchik <66141166+harvanchik@users.noreply.github.com>
2022-11-06 17:15:56 -0800

    fixed goals scored in 2OT or higher not showing scoring summary

--- src/app/components/boxscore/scoring-summary/scoring-summary.component.ts
+++ src/app/components/boxscore/scoring-summary/scoring-summary.component.ts
@@ -4,101 +4,100 @@ import { HockeyGoal } from 'src/app/models/HockeyGoal';
 import { ColorService } from 'src/app/services/color.service';
 
 @Component({
-    selector: 'app-scoring-summary',
-    templateUrl: './scoring-summary.component.html'
+  selector: 'app-scoring-summary',
+  templateUrl: './scoring-summary.component.html',
 })
 export class ScoringSummaryComponent implements OnInit {
+  @Input() boxScore = new BoxScore();
+  @Input() pending = true;
 
-    @Input() boxScore = new BoxScore();
-    @Input() pending = true;
+  constructor(private colorService: ColorService) {}
 
-    constructor(
-        private colorService: ColorService
-    ) { }
+  ngOnInit(): void {}
 
-    ngOnInit(): void { }
+  /**
+   * Get the number of goals scored by a team.  Optionally, a period can be specified to filter number of goals by period.
+   * @param team The team to get goals for.
+   * @param period The period to filter goals by (null if all periods).
+   * @returns the number of goals scored by the team
+   */
+  public getNumberOfGoals(team: 'away' | 'home', period?: number): number {
+    // if box score not loaded yet, return 0
+    if (!this.boxScore?.shots) return 0;
+    // is overtime?
+    const isOT = period && period > 3;
+    // return number of goals in box score for a team and period
+    return this.boxScore?.goals
+      ?.filter(goal => !goal.isDisallowed)
+      ?.filter(goal => goal.team.toLowerCase() === team)
+      ?.filter(goal => (!period || isOT ? goal.period > 3 : goal.period === period))?.length;
+  }
 
-    /**
-     * Get the number of goals scored by a team.  Optionally, a period can be specified to filter number of goals by period.
-     * @param team The team to get goals for.
-     * @param period The period to filter goals by (null if all periods).
-     * @returns the number of goals scored by the team
-     */
-    public getNumberOfGoals(team: 'away' | 'home', period?: number): number {
-        // if box score not loaded yet, return 0
-        if (!this.boxScore?.shots) return 0;
-        // return number of goals in box score for a team and period
-        return this.boxScore?.goals
-            ?.filter(goal => !goal.isDisallowed)
-            ?.filter(goal => goal.team.toLowerCase() === team)
-            ?.filter(goal => !period || goal.period === period)
-            ?.length;
-    }
+  /**
+   * Get the number of shots on goal for a team in a period.
+   * @param team The team to get shots on goal for.
+   * @param period The period to get shots on goal for (null if all periods).
+   * @returns the number of shots on goal
+   */
+  public getShotsOnGoal(team: 'away' | 'home', period?: number): number {
+    // if box score not loaded yet, return 0
+    if (!this.boxScore?.shots) return 0;
+    // return number of shots on goal in box score for a team and period
+    return (
+      // get number of shots on goal
+      this.boxScore?.shots
+        // filter by team
+        ?.filter(shot => shot?.team?.toLowerCase() === team)
+        // filter by period if specified
+        ?.filter(shot => !period || shot?.period === period)
+        // filter out shots that were blocked
+        ?.filter(shot => !shot?.shotBlocker?.id)
+        // filter out shots that were taken on a disallowed goal
+        ?.filter(shot => !this.getGoal(shot?.goal?.id)?.isDisallowed)?.length
+    );
+  }
 
-    /**
-     * Get the number of shots on goal for a team in a period.
-     * @param team The team to get shots on goal for.
-     * @param period The period to get shots on goal for (null if all periods).
-     * @returns the number of shots on goal
-     */
-    public getShotsOnGoal(team: 'away' | 'home', period?: number): number {
-        // if box score not loaded yet, return 0
-        if (!this.boxScore?.shots) return 0;
-        // return number of shots on goal in box score for a team and period
-        return this.boxScore?.shots
-            // filter by team
-            ?.filter(shot => shot?.team?.toLowerCase() === team)
-            // filter by period if specified
-            ?.filter(shot => !period || shot?.period === period)
-            // filter out shots that were blocked
-            ?.filter(shot => !shot?.shotBlocker?.id)
-            // filter out shots that were taken on a disallowed goal
-            ?.filter(shot => !this.getGoal(shot?.goal?.id)?.isDisallowed)
-            // get number of shots on goal
-            ?.length;
-    }
+  /**
+   * Get a hockey goal by its id.
+   * @param id The id of the goal.
+   * @returns the hockey goal
+   */
+  public getGoal(id: string): HockeyGoal {
+    return this.boxScore?.goals?.find(goal => goal?.id === id);
+  }
 
-    /**
-     * Get a hockey goal by its id.
-     * @param id The id of the goal.
-     * @returns the hockey goal
-     */
-    public getGoal(id: string): HockeyGoal {
-        return this.boxScore?.goals?.find(goal => goal?.id === id);
-    }
+  /**
+   * Get the tippy tooltip text for a team in the period summary.
+   * @param team The team to get the tooltip for.
+   * @returns the tooltip text
+   */
+  public getTeamNameTooltip(team: 'away' | 'home'): string {
+    // if away team but no team set yet, return 'Away Team'
+    if (team === 'away' && !this.boxScore?.awayTeam) return 'Away Team';
+    // if away team set, return away team's full name
+    if (team === 'away') return (this.boxScore?.awayTeam?.location || '') + ' ' + (this.boxScore?.awayTeam?.name || '');
+    // if home team but no team set yet, return 'Home Team'
+    if (!this.boxScore?.homeTeam) return 'Home Team';
+    // if home team set, return home team's full name
+    return (this.boxScore?.homeTeam?.location || '') + ' ' + (this.boxScore?.homeTeam?.name || '');
+  }
 
-    /**
-     * Get the tippy tooltip text for a team in the period summary.
-     * @param team The team to get the tooltip for.
-     * @returns the tooltip text
-     */
-    public getTeamNameTooltip(team: 'away' | 'home'): string {
-        // if away team but no team set yet, return 'Away Team'
-        if (team === 'away' && !this.boxScore?.awayTeam) return 'Away Team';
-        // if away team set, return away team's full name
-        if (team === 'away') return (this.boxScore?.awayTeam?.location || '') + ' ' + (this.boxScore?.awayTeam?.name || '');
-        // if home team but no team set yet, return 'Home Team'
-        if (!this.boxScore?.homeTeam) return 'Home Team';
-        // if home team set, return home team's full name
-        return (this.boxScore?.homeTeam?.location || '') + ' ' + (this.boxScore?.homeTeam?.name || '');
-    }
+  /**
+   * Get the number of periods played, excluding overtime periods past the first one.
+   */
+  public getNumberOfPeriods(): number {
+    // if box score not loaded yet, return 0
+    if (!this.boxScore?.periods) return 3;
+    // if an overtime goal exits, return 4; otherwise, return 3
+    return this.boxScore?.goals?.find(goal => goal.period > 3) ? 4 : 3;
+  }
 
-    /**
-     * Get the number of periods played, excluding overtime periods past the first one.
-     */
-    public getNumberOfPeriods(): number {
-        // if box score not loaded yet, return 0
-        if (!this.boxScore?.periods) return 3;
-        // if an overtime goal exits, return 4; otherwise, return 3
-        return this.boxScore?.goals?.find(goal => goal.period > 3) ? 4 : 3;
-    }
-
-    /**
-     * Get the color that contrasts the background color.
-     * @param backgroundColor The background color.
-     * @returns the contrast color
-     */
-    public getContrastingColor(backgroundColor: string): string {
-        return this.colorService.getContrast(backgroundColor);
-    }
+  /**
+   * Get the color that contrasts the background color.
+   * @param backgroundColor The background color.
+   * @returns the contrast color
+   */
+  public getContrastingColor(backgroundColor: string): string {
+    return this.colorService.getContrast(backgroundColor);
+  }
 }
